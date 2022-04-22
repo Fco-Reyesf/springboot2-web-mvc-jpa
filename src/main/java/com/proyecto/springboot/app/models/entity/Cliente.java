e910419b50788c1d24a652aaf1532c4c4ef98b95
Fco-Reyesf <freyes.info@gmail.com>
2022-04-21 23:39:55 -0400

    creando entidad cliente

--- src/main/java/com/proyecto/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/proyecto/springboot/app/models/entity/Cliente.java
@@ -3,11 +3,14 @@ package com.proyecto.springboot.app.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 // clase pojo que esta mapeada a una entidad
 
@@ -19,11 +22,62 @@ public class Cliente implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
 	
 	private String nombre;
 	private String apellido;
 	private String mail;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
 	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
 }
