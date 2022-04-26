fffed14b5a70a54e13c790f531a318a7cd937e46
Fco-Reyesf <freyes.info@gmail.com>
2022-04-25 22:30:10 -0400

    dao de cliente

---
+++ src/main/java/com/proyecto/springboot/app/dao/ClienteDaoImplement.java
@@ -0,0 +1,43 @@
+package com.proyecto.springboot.app.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.proyecto.springboot.app.models.entity.Cliente;
+
+
+/**
+ * 
+ * repository, marca persistencia de los datos, marca como un componente de spring
+ * marcar los detalles de los errores
+ *
+ */
+
+@Repository
+public class ClienteDaoImplement implements IClienteDao {
+
+	/*
+	 * maneja el ciclo de vida, persistencia de los datos.
+	 * las actualiza, elimina, consulta, operaciones a la bd
+	 * pero como objetos, las consultas van a la clase entity, no a la tabla de la BD
+	 */
+	private EntityManager em;
+	
+	/*
+	 * transactional, solo de lectura.
+	 */
+	
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+}
---
+++ src/main/java/com/proyecto/springboot/app/dao/IClienteDao.java
@@ -0,0 +1,11 @@
+package com.proyecto.springboot.app.dao;
+
+import java.util.List;
+
+import com.proyecto.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao {
+
+	public List<Cliente> findAll();
+	
+}
