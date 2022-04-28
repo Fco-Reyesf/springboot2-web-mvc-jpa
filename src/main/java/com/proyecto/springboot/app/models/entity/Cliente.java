3e8b877338a8ea43a85968c08ecc7f362ba149e8
Fco-Reyesf <freyes.info@gmail.com>
2022-04-28 00:07:37 -0400

    fecha en form

--- src/main/java/com/proyecto/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/proyecto/springboot/app/models/entity/Cliente.java
@@ -8,10 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 // clase pojo que esta mapeada a una entidad
 
@@ -31,11 +32,12 @@ public class Cliente implements Serializable{
 	private String apellido;
 	private String mail;
 	
+	
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 	
-	@PrePersist
 	public void prePersist() {
 		createAt = new Date();
 	}
