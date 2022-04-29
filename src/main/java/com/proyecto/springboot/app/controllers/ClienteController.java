a00115d45d8e181dfe7c5fbff4a05916653a13a2
Fco-Reyesf <freyes.info@gmail.com>
2022-04-28 22:05:55 -0400

    validacion datos form

--- src/main/java/com/proyecto/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/proyecto/springboot/app/controllers/ClienteController.java
@@ -2,10 +2,13 @@ package com.proyecto.springboot.app.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,9 +36,18 @@ public class ClienteController {
 		model.put("cliente", cliente);
 		return "form";
 	}
+	/*
+	 *  -- bindingresult siempre va junto al objeto que es validado.
+	 *  -- En guardar no se pasa el cliente a listar cuando encuentra un error, este lo envia por debajo
+	 * siempre y cuando se llaman de la misma manera, si no se llama igual ocupar @modelatributte("nombre-objeto") 
+	 */
 	
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Listado de clientes");
+			return "form";
+		}
 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}
