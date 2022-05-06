05a7687975c37a8e8e03d2d018b7ca3acf0e1791
Fco-Reyesf <freyes.info@gmail.com>
2022-05-05 23:13:25 -0400

    paginable parte 2

---
+++ src/main/java/com/proyecto/springboot/app/utils/paginator/PageItem.java
@@ -0,0 +1,31 @@
+package com.proyecto.springboot.app.utils.paginator;
+
+public class PageItem {
+
+	private int numero;
+	private boolean paginaActual;
+	
+	public PageItem(int numero, boolean paginaActual) {
+		this.numero = numero;
+		this.paginaActual = paginaActual;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public boolean isPaginaActual() {
+		return paginaActual;
+	}
+
+	public void setPaginaActual(boolean paginaActual) {
+		this.paginaActual = paginaActual;
+	}
+	
+	
+	
+}
