e4faf320bdae9fd0f0dba31642dc0d5a0677607e
Fco-Reyesf <freyes.info@gmail.com>
2022-05-09 22:31:14 -0400

    insertar fotos

--- src/main/java/com/proyecto/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/proyecto/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,9 @@
 package com.proyecto.springboot.app.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.proyecto.springboot.app.models.entity.Cliente;
@@ -55,10 +60,23 @@ public class ClienteController {
 	 */
 	
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, @RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Listado de clientes");
 			return "form";
+		}
+		if (!foto.isEmpty()) {
+			Path directorioFotos = Paths.get("src//main//resources//static//uploads");
+			String directorioPath = directorioFotos.toFile().getAbsolutePath();
+			try {
+				byte[] bytes = foto.getBytes();
+				Path rutaCompleta =Paths.get(directorioPath + "//" + foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				flash.addFlashAttribute("info","se ha subido correctamente" + foto.getOriginalFilename());
+				cliente.setFoto(foto.getOriginalFilename());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		String mensajeFlash = (cliente.getId() != null)? "Cliente editado con exito" : "Cliente creado con exito";
 		clienteservice.save(cliente);
