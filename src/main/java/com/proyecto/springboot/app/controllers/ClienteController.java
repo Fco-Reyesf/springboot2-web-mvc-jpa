d4a7b4482fe4d445874debb52f571485ebeb4621
Fco-Reyesf <freyes.info@gmail.com>
2022-05-09 23:49:51 -0400

    controlador usuario id

--- src/main/java/com/proyecto/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/proyecto/springboot/app/controllers/ClienteController.java
@@ -35,6 +35,18 @@ public class ClienteController {
 	@Autowired
 	private IClienteService clienteservice;
 	
+	@RequestMapping(value = "/ver/{id}")
+	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash ) {
+		Cliente cliente = clienteservice.findById(id);
+		if(cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalle del cliente : " + cliente.getNombre());
+		return "ver";
+	}
+	
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
 		Pageable pageRequest = PageRequest.of(page, 4);
