959c8f643ac04a2c79b2ae7d2d41cfad9e6ce8b9
Fco-Reyesf <freyes.info@gmail.com>
2022-05-16 21:58:15 -0400

    entity item factura

--- src/main/java/com/proyecto/springboot/app/models/entity/Factura.java
+++ src/main/java/com/proyecto/springboot/app/models/entity/Factura.java
@@ -1,14 +1,18 @@
 package com.proyecto.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
@@ -18,6 +22,13 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "facturas")
 public class Factura implements Serializable {
+
+	
+	
+	public Factura() {
+		super();
+		this.items = new ArrayList<ItemFactura>();
+	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,6 +49,18 @@ public class Factura implements Serializable {
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id")
+	private List<ItemFactura> items;
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
 
 	public long getId() {
 		return id;
@@ -81,5 +104,9 @@ public class Factura implements Serializable {
 	}
 	
 	private static final long serialVersionUID = 1L;
+	
+	public void addItems(ItemFactura itemsFactura) {
+		this.items.add(itemsFactura); 
+	}
 	
 }
---
+++ src/main/java/com/proyecto/springboot/app/models/entity/ItemFactura.java
@@ -0,0 +1,43 @@
+package com.proyecto.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "factura_items")
+public class ItemFactura implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer cantidad;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public Long calcularImporte() {
+		return cantidad.longValue();
+	}
+
+	public static final long serialVersionUID = 1L;
+
+}
