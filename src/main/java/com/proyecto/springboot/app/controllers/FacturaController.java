7fb3308a961e88284b576dfba527dd4f41a1748d
Fco-Reyesf <freyes.info@gmail.com>
2022-05-18 22:48:32 -0400

    vista factura

--- src/main/java/com/proyecto/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/proyecto/springboot/app/controllers/FacturaController.java
@@ -4,10 +4,10 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.proyecto.springboot.app.models.entity.Cliente;
@@ -16,6 +16,7 @@ import com.proyecto.springboot.app.models.service.IClienteService;
 
 @Controller
 @RequestMapping("/factura")
+@SessionAttributes("factura")
 public class FacturaController {
 
 	@Autowired
